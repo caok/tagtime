24a61b0f52ab723a078a54bb819012e7797217cc
xiongbo <xiongbo027@gmail.com>
2015-10-13 18:09:50 +0800

    add tag time to issue

--- extensions/chrome/js/tag_issues.js
+++ extensions/chrome/js/tag_issues.js
@@ -1,7 +1,22 @@
 var TagIssues = (function($){
   var issues, html;
-  var jquery_mapping = {};
+  var jq_mapping = {};
 
-  var init = function(){
+  var init_jquery_mapping = function(){
+    jq_mapping.$issues = $(".issue-meta")
+  };
+
+  init_jquery_mapping();
+
+  var init_issues = function(){ 
+    jq_mapping.$issues.each(function(){ 
+      $('<span />').text("2h:30m").addClass("tag_time").appendTo($(this)); 
+    });
+  };
+
+  return {
+    init: init_issues
   }
 })(jQuery);
+
+TagIssues.init();

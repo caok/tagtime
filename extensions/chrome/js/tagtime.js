b773ab0e791d91e2e3331a5f7d7a9e466a3c767f
xiongbo <xiongbo027@gmail.com>
2015-10-16 22:03:21 +0800

    add vue post demo of tagtime

--- extensions/chrome/js/tagtime.js
+++ extensions/chrome/js/tagtime.js
@@ -1,2 +1,22 @@
-var TagTime = (function($){ 
-})(jQuery);
+var createURL = "http://localhost:3002/apis/issues" 
+
+var tagTime = new Vue({
+  el: "#tagtime",
+  data: {
+    timeTip: "time format: 1h30m"
+  },
+  methods: { 
+    createTag: function(e){
+      console.log("start!")
+      $.ajax({
+        type: "POST",
+        url: createURL,
+        data: {},
+        success: function(data){
+          console.log(data.notice)
+        },
+        dataType: 'JSON'
+      });
+    } 
+  }
+}); 

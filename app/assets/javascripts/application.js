d13a639eda019af1487a5d119f70adac5d0572cc
xiongbo <xiongbo027@gmail.com>
2015-10-16 15:06:00 +0800

    add vue js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require vue
 //= require turbolinks
-//= require_tree .
--- app/controllers/apis/issues_controller.rb
+++ app/controllers/apis/issues_controller.rb
@@ -2,6 +2,7 @@ module Apis
   class IssuesController < BaseController 
     def index 
       @issues = Issue.all
+      render file: 'issues/index.json.jbuilder'
     end 
   end
 end

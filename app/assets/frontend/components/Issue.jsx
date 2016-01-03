da07194840f861653b1e0f02e3c4b6fed523fdf8
xiongbo <xiongbo027@gmail.com>
2016-01-03 17:47:14 +0800

    Refs #11 fix update

--- app/assets/frontend/components/Issue.jsx
+++ app/assets/frontend/components/Issue.jsx
@@ -6,7 +6,7 @@ export default class Issue extends React.Component {
         <span className='time'>{this.props.happenedAt}</span>
         <span className='time'>{this.props.time}</span>
         <span className='operation' onClick={this.deleteIssue.bind(this, this.props.id)}> [delete]</span>
-        <input type='text' id='issue_editor' onKeyDown={ this.updateIssue.bind(this, this.props.id) } onBlur={this.hiddenEditor} style={{display: 'none'}} />
+        <input type='text' className="issue_editor" onKeyDown={this.updateIssue.bind(this, this.props.id)} onBlur={this.hiddenEditor} style={{display: 'none'}} />
       </li>
     );
   }
@@ -15,22 +15,26 @@ export default class Issue extends React.Component {
     this.props.deleteIssue(tagId);
   }
 
-  editIssue(event) {
-    $("#issue_editor").css({"display":  "", "width": "800px"});
-    $("#issue_editor").css("left", event.clientX);
-    $("#issue_editor").css("top", event.clientY);
-    $("#issue_editor").val("with " + this.props.content + " @" + this.props.project_name + " for" + " #" + this.props.number + " " + this.props.time);
+  editIssue(event) { 
+    $(".issue_editor").css({"display":  "none"});
+    var editor = $(event.target).parent().children(".issue_editor")
+
+    editor.css({"display":  "", "width": "800px"});
+    editor.css("left", event.clientX);
+    editor.css("top", event.clientY);
+    editor.val("with " + this.props.content + " @" + this.props.project_name + " for" + " #" + this.props.number + " " + this.props.time);
   }
 
   hiddenEditor(event) {
-    $("#issue_editor").css({"display":  "none"});
+    $(event.target).css({"display":  "none"});
   }
 
   updateIssue(tagId, event) { 
     if(event.keyCode == 13) {
-      $("#issue_editor").css({"display":  "none"}); 
-      this.props.updateIssue(this.props.id, $("#issue_editor").val());
-      $("#issue_editor").val('');
+      var editor = $(event.target);
+      editor.css({"display":  "none"}); 
+      this.props.updateIssue(this.props.id, editor.val());
+      editor.val('');
     }
   }
 }

9ff269ed25e0cd8224ccf2a39fd64cdf06367f7d
xiongbo <xiongbo027@gmail.com>
2016-01-06 20:19:08 +0800

    Refs #36

--- app/assets/frontend/components/IssueList.jsx
+++ app/assets/frontend/components/IssueList.jsx
@@ -9,13 +9,21 @@ export default class IssueList extends React.Component {
     this.props.updateIssue(tagId, tagValue);
   }
 
+  loadMore() { 
+    this.props.loadMore();
+  }
+
   render() {
     let issues = this.props.issues.map(issue => <Issue key={issue.id} {...issue} deleteIssue={this.deleteIssue.bind(this)} updateIssue={this.updateIssue.bind(this)} />);
+
     return (
       <div className='issue_list'>
         <ul>
           {issues}
         </ul>
+        <div style={{"textAlign": "center", "marginTop": "20px"}}>
+          <input type="button" value="Load More" onClick={this.loadMore.bind(this)} />
+        </div>
       </div>
     );
   }

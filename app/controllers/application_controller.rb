efc2d6ad11773716613f02523010a7ea00bcd414
xiongbo <xiongbo027@gmail.com>
2016-01-08 20:00:40 +0800

    Refs #33 update error log

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  rescue_from ::Exception, with: :error_occurred
   protect_from_forgery with: :exception
+
+  def error_occurred(e)
+    ErrorLog.create("system error", e.message)
+    return
+  end
 end
